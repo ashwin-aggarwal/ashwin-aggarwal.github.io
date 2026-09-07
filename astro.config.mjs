9963d495a3856040c0329c24b8438f7f85f331da
Ashwin Aggarwal <aka96@cornell.edu>
2026-09-07 15:54:15 -0400

    deployed

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ashwin-aggarwal.github.io',
-  base: '/',
   vite: {
     plugins: [tailwindcss()],
   },
